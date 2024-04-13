2ff09f7b62ae598450c708411309f2b6077f2e58
Natan Maia <natanmaiaplays@outlook.com>
2024-04-12 22:52:05 -0300

    vmath com classe vec2 e funções

--- GeometriaComputacional/src/vmath.cpp
+++ GeometriaComputacional/src/vmath.cpp
@@ -1,40 +1,5 @@
 #include <cmath>
-
-class vec2 {
-public:
-	// presets
-	static const vec2 ZERO;
-	static const vec2 UP;
-	static const vec2 DOWN;
-
-	// variables
-	double x, y;
-
-	// constructor
-	vec2(double x_in, double y_in);
-	vec2(const vec2& v); //copy
-
-	// operators
-	//		with other vectors
-	friend vec2 operator+(const vec2& lhs, const vec2& rhs);
-	friend vec2 operator-(const vec2& lhs, const vec2& rhs);
-	//		with scalars
-	friend vec2 operator*(const double& scalar, const vec2& v);
-	friend vec2 operator*(const vec2& v, const double& scalar);
-	friend vec2 operator/(const vec2& v, const double& scalar);
-
-	// functions
-	double magSquared(); // faster
-	double mag();
-	double dot(const vec2& other);
-	double cross(const vec2& other);
-
-	vec2 normalized();
-	vec2 floored();
-	vec2 clampedPositive();
-	vec2 project(vec2& base);
-	vec2 reflect(vec2& normal);
-};
+#include "vmath.h"
 
 // constructor
 vec2::vec2(double x_in, double y_in) {
